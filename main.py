ac1a7a66b11cbdd8a3b0b367e7ede81fcae4f941
Chris Fistouris <cgf20@ic.ac.uk>
2022-02-28 19:22:21 +0200

    Chnage name, imprt modules

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 """Statistical analysis for vaccines."""
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+
+file = open('vaccination by age group.csv')
+
+csvreader = csv.reader(file)
 
 class Data_Analysis:
     
